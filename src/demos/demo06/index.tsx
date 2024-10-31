0f8eb6b607619ff01c15b9902637b64c581644cb
panzheng <zheng.pan@js.design>
2024-10-31 19:54:56 +0800

    Fix demo06 INVALID_OPERATION

--- src/demos/demo06/index.tsx
+++ src/demos/demo06/index.tsx
@@ -26,31 +26,32 @@ export default function Demo06() {
       gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
       // 绘制x轴和y轴
-      gl.enableVertexAttribArray(positionAttributeLocation);
-      const lines = [5, 5, gl.canvas.width / 2, 5, 5, 5, 5, gl.canvas.height / 2];
       const positionBuffer = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+      const lines = [5, 5, gl.canvas.width / 2, 5, 5, 5, 5, gl.canvas.height / 2];
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lines), gl.STATIC_DRAW);
-
+      gl.enableVertexAttribArray(positionAttributeLocation);
       gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
       gl.uniform4f(colorUniformLocation, 0, 0, 0, 1);
 
       gl.drawArrays(gl.LINES, 0, 4);
 
       // 绘制原点
-      gl.enableVertexAttribArray(pointSizeAttributeLocation);
-
       gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([5, 5]), gl.STATIC_DRAW);
+      gl.enableVertexAttribArray(positionAttributeLocation);
       gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
       const pointSizeBuffer = gl.createBuffer();
       gl.bindBuffer(gl.ARRAY_BUFFER, pointSizeBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([10]), gl.STATIC_DRAW);
+      gl.enableVertexAttribArray(pointSizeAttributeLocation);
       gl.vertexAttribPointer(pointSizeAttributeLocation, 1, gl.FLOAT, false, 0, 0);
-
       gl.uniform4f(colorUniformLocation, 254 / 255, 82 / 255, 33 / 255, 1);
       gl.drawArrays(gl.POINTS, 0, 1);
+      gl.deleteBuffer(pointSizeBuffer);
+      // fix INVALID_OPERATION: drawArrays: no buffer is bound to enabled attribute
+      gl.disableVertexAttribArray(pointSizeAttributeLocation);
 
       // 绘制O 椭圆
       gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -69,9 +70,9 @@ export default function Demo06() {
         points.push(x + 20, y + 20);
       }
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+      gl.enableVertexAttribArray(positionAttributeLocation);
       gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
       gl.uniform4f(colorUniformLocation, 100 / 255, 150 / 255, 238 / 255, 1);
-
       gl.drawArrays(gl.LINE_LOOP, 0, 100);
 
       // 绘制箭头
@@ -91,9 +92,9 @@ export default function Demo06() {
         5,
       ];
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrows), gl.STATIC_DRAW);
+      gl.enableVertexAttribArray(positionAttributeLocation);
       gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
       gl.uniform4f(colorUniformLocation, 0, 0, 0, 1);
-
       gl.drawArrays(gl.TRIANGLES, 0, 6);
     }
   };
